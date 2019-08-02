692c52a878829317d7052afa55bd12206e0adf0f
Ville Aikas <vaikas@google.com>
2019-08-02 08:06:41 -0700

    sed to support both mac and linux

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -18,6 +18,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Let sed work on mac.
+if [ "$(uname)" == "Darwin" ]; then
+  sed=gsed
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  sed=sed
+fi
+
 source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/library.sh
 
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${REPO_ROOT_DIR}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
@@ -30,21 +37,22 @@ KNATIVE_CODEGEN_PKG=${KNATIVE_CODEGEN_PKG:-$(cd ${REPO_ROOT_DIR}; ls -d -1 ./ven
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/GoogleCloudPlatform/cloud-run-events/pkg/client github.com/GoogleCloudPlatform/cloud-run-events/pkg/apis \
-  "pubsub:v1alpha1 messaging:v1alpha1" \
+  "pubsub:v1alpha1 messaging:v1alpha1 events:v1alpha1" \
   --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt
 
 # Knative Injection
 ${KNATIVE_CODEGEN_PKG}/hack/generate-knative.sh "injection" \
   github.com/GoogleCloudPlatform/cloud-run-events/pkg/client github.com/GoogleCloudPlatform/cloud-run-events/pkg/apis \
-  "pubsub:v1alpha1 messaging:v1alpha1" \
+  "pubsub:v1alpha1 messaging:v1alpha1 events:v1alpha1" \
   --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt
 
 # Because the kubernetes code generators force pacakges to lowercase, the update-deps script will be confused for
-# imports of github.com/googlecloudplatform/... We will update that to use the correct casing in the generated code.
+# imports of github.com/GoogleCloudPlatform/... We will update that to use the correct casing in the generated code.
 # The following will find all files (not directories, specified by -type f) under ${REPO_ROOT_DIR}/pkg/client, and
-# perform a sed command to replace "googlecloudplatform" with "GoogleCloudPlatform" on each file it finds.
+# perform a sed command to replace "GoogleCloudPlatform" with "GoogleCloudPlatform" on each file it finds.
 
-find ${REPO_ROOT_DIR}/pkg/client -type f -exec sed -i '' -e 's/googlecloudplatform/GoogleCloudPlatform/g' {} \;
+find ${REPO_ROOT_DIR}/pkg/client -type f -exec $sed -i 's@googlecloudplatform@GoogleCloudPlatform@g' {} \;
 
 # Make sure our dependencies are up-to-date
+echo "updating deps"
 ${REPO_ROOT_DIR}/hack/update-deps.sh
